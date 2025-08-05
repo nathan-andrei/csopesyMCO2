f1bb649b216aa2fded85709364ed973de3b47f1c
nathan-andrei <nathan_andrei_asnan@dlsu.edu.ph>
2025-08-05 15:31:08 +0800

    Added extra funcs to cores and made it work on threads

--- cpuCore.h
+++ cpuCore.h
@@ -4,7 +4,7 @@
 
 #include "console.h"
 
-
+using std::thread;
 
 using console::Console;
 
@@ -15,20 +15,29 @@ class cpuCore {
     bool running = false; // Flag to control the core's operation
     int delayPerExec = 0; // Delay in milliseconds for each execution step
 
+    thread worker;
+
     cpuCore(int id, int execDelay) : coreId(id), delayPerExec(execDelay) {}
 
     void startProcess(Console* console){
         running = true;
         processLoop(console); // Start the process loop in a separate thread
+
+        worker = thread(processLoop, ref(console));
+
     }
 
     void stop() {
         running = false;
+        if (worker.joinable()) {
+            worker.join();
+        }
     }
 
     void processLoop(Console* console) {
         processToRun = console; // Set the process to run on this core
-        while (running) { 
+        bool finishedFlag = false;
+        while (running && !finishedFlag) { 
             if(!console->process.commands.empty()) {
                 try{
                     console->handleInput(console->process.commands.front());
@@ -36,17 +45,20 @@ class cpuCore {
                 }catch(std::exception e){
                     cout << "error with somethign?? " << e.what() << endl;
                 };
+                console->process.currLine += 1;
             }
-            console->process.currLine += 1;
+            else{
+                console->process.end();
+            }
 
             std::this_thread::sleep_for(std::chrono::milliseconds(delayPerExec));
-        
-
-            {
-                console->process.end();
-                stop();
-            }
         }
+
+        stop(); //this might resource deadlock lol.
+    }
+
+    bool processFinished(){
+        return processToRun->process.commands.empty();
     }
 };
 
